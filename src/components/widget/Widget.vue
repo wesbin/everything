623ed876262cfd0c719692a7edc9fc38c1102236
wesbin <leegosoo11@naver.com>
2021-12-31 11:49:27 +0900

    mixin => composable 완료

--- src/components/widget/Widget.vue
+++ src/components/widget/Widget.vue
@@ -1,16 +1,16 @@
 <template>
   <div ref="float" class="widget" :style="widget.style">
-    <component :is="componentLoader" :widget="widget" @floatMousedown="$_float_dragMouseDown"></component>
+    <component :is="componentLoader" :widget="widget" @floatMousedown="dragMouseDown"></component>
+    <!--    <component :is="componentLoader" :widget="widget" @floatMousedown="$_float_dragMouseDown"></component>-->
   </div>
 </template>
 
 <script>
 import { computed, defineAsyncComponent } from 'vue';
-import widgetFloat from '@/mixins/widgetFloat';
+import floatWidget from '@/composables/floatWidget';
 
 export default {
   name: 'Widget',
-  mixins: [widgetFloat],
   props: {
     widget: {
       type: Object,
@@ -18,6 +18,9 @@ export default {
     },
   },
   setup(props) {
+    // 지금은 사실 따로 빼서 관리하는게 의미가 없긴 해
+    const { dragMouseDown, float } = floatWidget(props.widget);
+
     const componentLoader = computed(() => {
       if (props.widget.visible) {
         return defineAsyncComponent(() => import(`@/components/widget/${props.widget.type}`));
@@ -26,6 +29,8 @@ export default {
     });
     return {
       componentLoader,
+      dragMouseDown,
+      float,
     };
   },
 };
--- src/composables/floatWidget.js
+++ src/composables/floatWidget.js
@@ -1,21 +1,21 @@
 import { computed, onMounted, reactive, ref, toRefs } from 'vue';
 import { useStore } from 'vuex';
 
-export default () => {
+export default (widget) => {
   // data
-  const positions = toRefs(
-    reactive({
-      clientX: '',
-      clientY: '',
-      movementX: '',
-      movementY: '',
-    })
-  );
+  const positions = reactive({
+    clientX: '',
+    clientY: '',
+    movementX: '',
+    movementY: '',
+  });
 
   // vuex
   const store = useStore();
   const getTopIndex = computed(() => store.getters['widget/getTopIndex']);
-  const setDrag =
+  const setDrag = () => store.commit('widget/setDrag');
+  const upIndex = () => store.commit('widget/upIndex');
+  const updateWidget = (widget) => store.commit('widget/updateWidget', widget);
 
   // refs
   const float = ref(null);
@@ -23,76 +23,71 @@ export default () => {
   // methods
   const dragMouseDown = (event) => {
     event.preventDefault();
-    this.setDrag(false);
+    setDrag(false);
     // get the mouse cursor position at startup:
-    this.positions.clientX = event.clientX;
-    this.positions.clientY = event.clientY;
-    document.onmousemove = this.$_float_elementDrag;
-    document.onmouseup = this.$_float_closeDragElement;
+    positions.clientX = event.clientX;
+    positions.clientY = event.clientY;
+    document.onmousemove = elementDrag;
+    document.onmouseup = closeDragElement;
   };
 
   const elementDrag = (event) => {
     event.preventDefault();
-    this.setDrag(true);
-    this.positions.movementX = this.positions.clientX - event.clientX;
-    this.positions.movementY = this.positions.clientY - event.clientY;
-    this.positions.clientX = event.clientX;
-    this.positions.clientY = event.clientY;
+    setDrag(true);
+    positions.movementX = positions.clientX - event.clientX;
+    positions.movementY = positions.clientY - event.clientY;
+    positions.clientX = event.clientX;
+    positions.clientY = event.clientY;
     // set the element's new position:
-    this.$refs.float.style.top = this.$refs.float.offsetTop - this.positions.movementY + 'px';
-    this.$refs.float.style.left = this.$refs.float.offsetLeft - this.positions.movementX + 'px';
-  }
+    float.value.style.top = float.value.offsetTop - positions.movementY + 'px';
+    float.value.style.left = float.value.offsetLeft - positions.movementX + 'px';
+  };
 
   const closeDragElement = () => {
-    this.$_float_rePositioning();
+    rePositioning();
     document.onmouseup = null;
     document.onmousemove = null;
-  }
+  };
 
   const rePositioning = () => {
     // 플로팅 객체의 너비, 높이 등
-    const rect = this.$refs.float.getBoundingClientRect();
+    const rect = float.value.getBoundingClientRect();
     // 화면 너비
     const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
     // 화면 높이
     const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
     // 좌측
     if (rect.left < 0) {
-      this.$refs.float.style.left = '0px';
+      float.value.style.left = '0px';
     }
     // 상단
     if (rect.top < 0) {
-      this.$refs.float.style.top = '0px';
+      float.value.style.top = '0px';
     }
     // 우측
     if (rect.right > vw) {
-      this.$refs.float.style.left = `${vw - rect.width}px`;
+      float.value.style.left = `${vw - rect.width}px`;
     }
     // 하단
     if (rect.bottom > vh) {
-      this.$refs.float.style.top = `${vh - rect.height}px`;
+      float.value.style.top = `${vh - rect.height}px`;
     }
-    this.$_float_savePosition(
-      this.$refs.float.style.left || '13%',
-      this.$refs.float.style.top || '13%',
-      rect.width,
-      rect.height
-    );
-  }
+    savePosition(float.value.style.left || '13%', float.value.style.top || '13%', rect.width, rect.height);
+  };
 
   const focusOn = () => {
-    this.upIndex();
-    this.updateWidget({
-      widget: this.widget,
+    upIndex();
+    updateWidget({
+      widget: widget,
       style: {
-        'z-index': this.getTopIndex,
+        'z-index': getTopIndex.value,
       },
     });
-  }
+  };
 
   const savePosition = (left, top, width, height) => {
-    this.updateWidget({
-      widget: this.widget,
+    updateWidget({
+      widget: widget,
       style: {
         left: left,
         top: top,
@@ -100,15 +95,16 @@ export default () => {
         height: height,
       },
     });
-  }
+  };
 
   onMounted(() => {
-    float.value.addEventListener('mousedown', () => {});
+    float.value.addEventListener('mousedown', () => {
+      focusOn();
+    });
   });
 
   return {
     float,
     dragMouseDown,
-
   };
 };
